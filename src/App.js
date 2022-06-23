8b1e61fa43415d1c242e1f0f20bbbbc979005ce9
sejal-m <sejalmaurya27@gmail.com>
2022-06-23 12:07:07 +0530

    beta commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,17 @@
-import React from "react";
-import { HashRouter as Router, Route, NavLink } from "react-router-dom";
-import SignUpForm from "./pages/SignUpForm";
-import SignInForm from "./pages/SignInForm";
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import Login from "./components/auth/Login.js";
 
-import "./App.css";
-
-function App () {
+function App() {
   return (
-    <Router basename="/react-auth-ui/">
-        <div className="App">
-          <div className="appAside" />
-          <div className="appForm">
-            <div className="pageSwitcher">
-              <NavLink
-                to="/sign-in"
-                activeClassName="pageSwitcherItem-active"
-                className="pageSwitcherItem"
-              >
-                Sign In
-              </NavLink>
-              <NavLink
-                exact
-                to="/"
-                activeClassName="pageSwitcherItem-active"
-                className="pageSwitcherItem"
-              >
-                Sign Up
-              </NavLink>
-            </div>
-
-            <div className="formTitle">
-              <NavLink
-                to="/sign-in"
-                activeClassName="formTitleLink-active"
-                className="formTitleLink"
-              >
-                Sign In
-              </NavLink>{" "}
-              or{" "}
-              <NavLink
-                exact
-                to="/"
-                activeClassName="formTitleLink-active"
-                className="formTitleLink"
-              >
-                Sign Up
-              </NavLink>
-            </div>
-
-            <Route exact path="/" component={SignUpForm} />
-            <Route path="/sign-in" component={SignInForm} />
-          </div>
-        </div>
-      </Router>
+    <Router>
+    <div className="App">
+      <Routes>
+      <Route path="/" element={<Login/>} />
+      </Routes>
+    </div>
+  </Router>
   );
 }
 
---
+++ src/components/auth/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../auth/Login.module.css'
+
+export default function login () {
+  return (
+    <div class="float-container">
+
+        <div class="float-child">
+          <div class="green">Float Column 1</div>
+        </div>
+        
+        <div class="float-child">
+          <div class="blue">Float Column 2</div>
+        </div>
+        
+</div>
+  );
+}
+
